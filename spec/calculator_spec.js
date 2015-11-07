297a6a42d0d0d1da43a25b27a161cd5150f89b6d
Germain Boue <boue.germain@gmail.com>
2015-11-06 20:33:31 -0800

    Subtract method added to specs and js

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -1,12 +1,30 @@
 describe("Calculator", function(){
 
-  it("should store the current value at all times", function(){
-    expect(Calculator.current).toBeDefined();
-    expect(Calculator.current).toEqual(0);
+  beforeEach(function(){
+    Calculator.current = 0;
   });
 
-  it("should add numbers", function(){
-    expect(Calculator.add(5)).toEqual(5);
-    expect(Calculator.add(5)).toEqual(10);
+  describe("When adding numbers", function(){
+
+    it("should store the current value at all times", function(){
+      expect(Calculator.current).toBeDefined();
+      expect(Calculator.current).toEqual(0);
+    });
+
+    it("should add numbers", function(){
+      expect(Calculator.add(5)).toEqual(5);
+      expect(Calculator.add(5)).toEqual(10);
+    });
+
+    it("should add any number of numbers", function(){
+      expect(Calculator.add(1,2,3)).toEqual(6);
+      expect(Calculator.add(1,2,3,4)).toEqual(16);
+    });
+  });
+
+  describe("When subtracting numbers", function(){
+    it("should subtract any number of numbers", function(){
+      expect(Calculator.subtract(5)).toEqual(-5);
+    });
   });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,20 @@ window.Calculator = {
   current: 0,
 
   add: function(num1){
-    this.current += num1;
+    var sum = this.current;
+    for (var i = 0, len = arguments.length; i < len; i++){
+      sum += arguments[i];
+    }
+    this.current = sum;
     return this.current;
+  },
+
+  subtract: function(){
+    var result = this.current;
+    for (var i = 0, len = arguments.length; i < len; i++){
+      result -= arguments[i];
+    }
+    this.current = result;
+    return result;
   }
 };
